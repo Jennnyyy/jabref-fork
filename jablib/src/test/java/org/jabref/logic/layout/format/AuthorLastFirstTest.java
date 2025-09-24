2fd80b9106dff985438cfb323a13787e673e940c
Jennifer_Flores <jennybyte2022@gmail.com>
2025-09-23 19:23:30 -0700

    Update AuthorLastFirstTest to use parameterized test

--- jablib/src/test/java/org/jabref/logic/layout/format/AuthorLastFirstTest.java
+++ jablib/src/test/java/org/jabref/logic/layout/format/AuthorLastFirstTest.java
@@ -2,31 +2,23 @@ package org.jabref.logic.layout.format;
 
 import org.jabref.logic.layout.LayoutFormatter;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class AuthorLastFirstTest {
 
-    @Test
-    void format() {
+    @ParameterizedTest
+    @CsvSource({
+            ",",
+            "Someone, Van Something,Van Something Someone",
+            "von Neumann, John and Black Brown, Peter, John von Neumann and Black Brown, Peter",
+            "von Neumann, John and Smith, John and Black Brown, Peter,von Neumann, John and Smith, John and Black Brown, Peter",
+            "von Neumann, John and Smith, John and Black Brown, Peter,John von Neumann and John Smith and Black Brown, Peter"
+    })
+    void format(String expected, String input) {
         LayoutFormatter a = new AuthorLastFirst();
-
-        // Empty case
-        assertEquals("", a.format(""));
-
-        // Single Names
-        assertEquals("Someone, Van Something", a.format("Van Something Someone"));
-
-        // Two names
-        assertEquals("von Neumann, John and Black Brown, Peter", a
-                .format("John von Neumann and Black Brown, Peter"));
-
-        // Three names
-        assertEquals("von Neumann, John and Smith, John and Black Brown, Peter", a
-                .format("von Neumann, John and Smith, John and Black Brown, Peter"));
-
-        assertEquals("von Neumann, John and Smith, John and Black Brown, Peter", a
-                .format("John von Neumann and John Smith and Black Brown, Peter"));
+        assertEquals(expected, a.format(input));
     }
 }
