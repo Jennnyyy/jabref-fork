0d689dce85eaf6824856ba42f2e8ff0b7ed20b2d
Jennifer_Flores <jennybyte2022@gmail.com>
2025-09-23 19:28:03 -0700

    Refactor RisKeywordsTest to use parameterized tests with Stream<Arguments>

--- jablib/src/test/java/org/jabref/logic/layout/format/RisKeywordsTest.java
+++ jablib/src/test/java/org/jabref/logic/layout/format/RisKeywordsTest.java
@@ -1,36 +1,33 @@
 package org.jabref.logic.layout.format;
 
-import org.jabref.logic.os.OS;
+import java.util.stream.Stream;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.jabref.logic.os.OS;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class RisKeywordsTest {
 
-    @Test
-    void empty() {
-        assertEquals("", new RisKeywords().format(""));
+
+
+public class RisKeywordsTest {
+
+    static Stream<Arguments> getKeywords() {
+        return Stream.of(
+                Arguments.of("", ""),
+                Arguments.of("", null),
+                Arguments.of("KW  - abcd", "abcd"),
+                Arguments.of("KW  - abcd" + OS.NEWLINE + "KW  - efg", "abcd, efg"),
+                Arguments.of("KW  - abcd" + OS.NEWLINE + "KW  - efg" + OS.NEWLINE
+                        + "KW  - hij" + OS.NEWLINE + "KW  - klm", "abcd, efg, hij, klm")
+        );
     }
 
-    @Test
-    void testNull() {
-        assertEquals("", new RisKeywords().format(null));
-    }
-
-    @Test
-    void singleKeyword() {
-        assertEquals("KW  - abcd", new RisKeywords().format("abcd"));
-    }
-
-    @Test
-    void twoKeywords() {
-        assertEquals("KW  - abcd" + OS.NEWLINE + "KW  - efg", new RisKeywords().format("abcd, efg"));
-    }
-
-    @Test
-    void multipleKeywords() {
-        assertEquals("KW  - abcd" + OS.NEWLINE + "KW  - efg" + OS.NEWLINE + "KW  - hij" + OS.NEWLINE
-                + "KW  - klm", new RisKeywords().format("abcd, efg, hij, klm"));
+    @ParameterizedTest
+    @MethodSource("getKeywords")
+    void differentNumOfKeywords(String expected, String input) {
+        assertEquals(expected, new RisKeywords().format(input));
     }
 }
